b6229f84b50b8b12a379a1188379b7279a4df3a3
gabriellavdk <gvandekooij@outlook.com>
2021-07-14 12:39:18 +0200

    De code voor het inladen van de juiste images en link + poster daarbij werkend gekregen met Javascript

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
 import { movies } from './movie.js';
 
-// const titleOfMovie = movies.map(item => item.Title);
-// const yearOfMovie = movies.map(item => item.Year);
-// const imdbIdOfMovie = movies.map(item => item.imdbID);
-// const typeOfMovie = movies.map(item => item.Type);
-// const posterOfMovie = movies.map(item => item.Poster);
-
 // Functions
 const searchArray = str => movies.filter(({ Title }, result) => result = Title.includes(str));
-const addImageToHtml = (image, url) => {
-	const menu = document.getElementById('overview-movies');
-	const newImg = document.createElement("img");
-	newImg.classList.add("");
-	newImg.src = image;
 
-	// link toevoegen naar imbd
-
-	menu.appendChild(newImg);
+const addImageToHtml = (movies) => {
+	movies.forEach(({ imdbID, Poster }) => {
+		const menu = document.getElementById('overview-movies');
+		const newLink = document.createElement("a");
+		const newImg = document.createElement("img");
+		newLink.href = ("https://www.imdb.com/title/" + imdbID);
+		newLink.target = "_blank";
+		newImg.classList.add("test");
+		newImg.src = Poster;
+		menu.appendChild(newLink);
+		newLink.appendChild(newImg);
+	})
 }
+
 
 
 // Constants
@@ -27,11 +26,11 @@ const xmenMovies = searchArray("X-Men");
 const princessMovies = searchArray("Princess");
 const batmanMovies = searchArray("Batman");
 
-console.log("New Movies: ", newMovies);
-console.log("Avengers Movies: ", avengersMovies);
-console.log("X-men Movies: ", xmenMovies);
-console.log("Princess Movies: ", princessMovies);
-console.log("Batman Movies: ", batmanMovies);
+// console.log("New Movies: ", newMovies);
+// console.log("Avengers Movies: ", avengersMovies);
+// console.log("X-men Movies: ", xmenMovies);
+// console.log("Princess Movies: ", princessMovies);
+// console.log("Batman Movies: ", batmanMovies);
 
-
+addImageToHtml(newMovies);
 
